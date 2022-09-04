5de9e51a4c80a85262e04c7c29319bec17416403
Michael Miller <millergm30@gmail.com>
2022-09-04 19:21:38 -0230

    gh-pages

---
+++ javascript/passGenerator.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const password = document.getElementById("password");
+
+function genPassword() {
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const passwordLength = 12;
+  let password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+  const copyText = document.getElementById("password");
+  copyText.select();
+  document.execCommand("copy");
+}
